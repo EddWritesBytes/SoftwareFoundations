cb015600e9f255e913592bd0f2b1880c51edff1a
EddWritesBytes <edd@onbyt.es>
2019-07-13 15:42:43 +0100

    Excercise 001 - nand

---
+++ exercise_001.v
@@ -0,0 +1,27 @@
+Definition nandb (b1:bool) (b2:bool) : bool :=
+    match b1 with
+    | false => true
+    | true => match b2 with
+              | false => true
+              | true => false
+              end
+    end.
+
+(* Truth table for testing *)
+Compute (nandb true false).
+Compute (nandb false false).
+Compute (nandb false true).
+Compute (nandb true true).
+
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.

94fe9ea415cbef71a27886768f227b7eb5da639f
EddWritesBytes <edd@onbyt.es>
2019-07-13 15:56:55 +0100

    Exercise 002 - and, extra solution to Exercise 001

--- exercise_001.v
+++ exercise_001.v
@@ -7,11 +7,24 @@ Definition nandb (b1:bool) (b2:bool) : bool :=
               end
     end.
 
+(* Found a non-nested method of patten matching! *)
+Definition nandb' (b1:bool) (b2:bool) : bool :=
+    match b1, b2 with
+    | true, true => false
+    | _, _ => true
+    end.
+
 (* Truth table for testing *)
 Compute (nandb true false).
 Compute (nandb false false).
 Compute (nandb false true).
 Compute (nandb true true).
+
+(* Truth table for testing *)
+Compute (nandb' true false).
+Compute (nandb' false false).
+Compute (nandb' false true).
+Compute (nandb' true true).
 
 
 Example test_nandb1: (nandb true false) = true.
@@ -24,4 +37,16 @@ Example test_nandb3: (nandb false true) = true.
 Proof. simpl. reflexivity. Qed.
 
 Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb1': (nandb' true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2': (nandb' false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3': (nandb' false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4': (nandb' true true) = false.
 Proof. simpl. reflexivity. Qed.
